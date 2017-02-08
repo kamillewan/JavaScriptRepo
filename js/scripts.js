64e40a084263056297ff6ccb5f3bd30248a3f95f
kamillewan <kamil_lewan@op.pl>
2017-02-08 19:03:53 +0100

    added js folder

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// plik scripts.js
+
+var name = prompt('Wpisz swoje imię');  //funkcja prompt powoduje wyskakiwania okienka z opcja wpisania 'zmiennej', w tym wypadku imienia
+alert('Witaj, ' + name);                //funkcja alert rowniez powoduje wysk. okienka, ale bez opcji wpisywania czegokolwiek
+console.log('Witaj, ' + name);          //funkcja wyswietlania w consoli
