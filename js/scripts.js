ec256ad79d75c49a8cf3c4f761419a020f1a162f
kamillewan <kamil_lewan@op.pl>
2017-02-17 17:00:44 +0100

    scripts.js modified for task 8.5

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,23 @@ var sideH = prompt('Podaj szerokość boku h');
 var triangleArea = sideA*sideH/2;
 
 alert('Pole trójkąta wynosi: ' + triangleArea);              
-console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + sideA + ' i wysokości h: ' + sideH + ' wynosi: ' + triangleArea);
+
+// TASK 8.5
+
+var a = 2, b = 4;
+var value = (a*a) + (2 * a * b) - (b*b);
+console.log('Wynik działania wynosi: ' + value);
+
+if (value > 0) {
+  console.log('Wynik działania jest dodatni')
+}
+
+else if (value < 0) {
+  console.log('Wynik działania jest ujemny')
+}
+
+if (value == 0) {
+  alert('Wynik jest równy: ' + value);
+  console.log('Wynik działania jest równy "0" ')
+}
